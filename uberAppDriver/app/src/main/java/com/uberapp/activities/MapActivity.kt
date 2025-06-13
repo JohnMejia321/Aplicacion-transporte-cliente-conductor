ef6156b6a97ba8f85ffbb898a9c78b9ad749a8e3
John-mejia <jmejia@innerconsulting.com>
2025-06-12 20:54:05 -0500

    add maps

--- uberAppDriver/app/src/main/java/com/uberapp/activities/MapActivity.kt
+++ uberAppDriver/app/src/main/java/com/uberapp/activities/MapActivity.kt
@@ -1,22 +1,32 @@
 package com.uberapp.activities
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.uberapp.R
+import com.uberapp.databinding.ActivityMapBinding
 
-class MapActivity : AppCompatActivity() {
-    
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityMapBinding
+    private var googleMap: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_map)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.map)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap= map
     }
 }
