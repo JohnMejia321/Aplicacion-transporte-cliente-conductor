4ddf1f1259580f8a5b8b16cd638e8854173f6fa4
John-mejia <jmejia@innerconsulting.com>
2025-06-12 20:38:03 -0500

    add sdk google maps app driver

--- uberAppDriver/app/src/main/java/com/uberapp/activities/MapActivity.kt
+++ uberAppDriver/app/src/main/java/com/uberapp/activities/MapActivity.kt
@@ -8,11 +8,12 @@ import androidx.core.view.WindowInsetsCompat
 import com.uberapp.R
 
 class MapActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_map)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.map)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
