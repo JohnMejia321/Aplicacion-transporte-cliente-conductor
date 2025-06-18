46d08323df622c3d8b3ce0d98035c4af85fabb74
John-mejia <jmejia@innerconsulting.com>
2025-06-17 21:07:06 -0500

    changes

--- uberAppDriver/app/src/main/java/com/uberapp/activities/MapActivity.kt
+++ uberAppDriver/app/src/main/java/com/uberapp/activities/MapActivity.kt
@@ -1,32 +1,173 @@
 package com.uberapp.activities
 
+import android.Manifest
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.easywaylocation.EasyWayLocation
+import com.example.easywaylocation.Listener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.uberapp.R
 import com.uberapp.databinding.ActivityMapBinding
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
 
     private lateinit var binding: ActivityMapBinding
     private var googleMap: GoogleMap? = null
+    private var easyWayLocation: EasyWayLocation? = null
+    private var myLocationLatLng: LatLng? = null
+    private var markerDriver: Marker? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 0
+            fastestInterval = 0
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+            smallestDisplacement = 1f
+        }
+
+        easyWayLocation = EasyWayLocation(this, locationRequest, false, false, this)
+
+        locationPermissions.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+
     }
 
+    val locationPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                when {
+                    permission.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                        Log.d("LOCALIZACION", "Permiso concedido")
+//                    easyWayLocation?.startLocation();
+                    }
+
+                    permission.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                        Log.d("LOCALIZACION", "Permiso concedido con limitacion")
+//                    easyWayLocation?.startLocation();
+                    }
+
+                    else -> {
+                        Log.d("LOCALIZACION", "Permiso no concedido")
+                    }
+                }
+            }
+
+        }
+
     override fun onMapReady(map: GoogleMap) {
-        googleMap= map
+        googleMap = map
     }
+
+
+
+    override fun locationOn() {
+    }
+
+    /*override fun currentLocation(location: Location?) {
+    }*/
+
+    override fun currentLocation(location: Location) { // ACTUALIZACION DE LA POSICION EN TIEMPO REAL
+        myLocationLatLng =
+            LatLng(location.latitude, location.longitude) // LAT Y LONG DE LA POSICION ACTUAL
+        googleMap?.moveCamera(
+            CameraUpdateFactory.newCameraPosition(
+                CameraPosition.builder().target(myLocationLatLng!!).zoom(17f).build()
+            )
+        )
+        //addMarker()
+    }
+
+
+    override fun locationCancelled() {
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        easyWayLocation?.startLocation();
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        easyWayLocation?.endUpdates();
+    }
+
+   /* private fun addMarker() {
+        val drawable = ContextCompat.getDrawable(applicationContext, R.drawable.uber_car)
+        val markerIcon = getMarkerFromDrawable(drawable!!)
+        if (markerDriver != null) {
+            markerDriver?.remove() // NO REDIBUJAR EL ICONO
+        }
+        if (myLocationLatLng != null) {
+            markerDriver = googleMap?.addMarker(
+                MarkerOptions()
+                    .position(myLocationLatLng!!)
+                    .anchor(0.5f, 0.5f)
+                    .flat(true)
+                    .icon(markerIcon)
+            )
+        }
+    }
+
+    private fun getMarkerFromDrawable(drawable: Drawable): BitmapDescriptor {
+        val canvas = Canvas()
+        val bitmap = Bitmap.createBitmap(
+            70,
+            150,
+            Bitmap.Config.ARGB_8888
+        )
+        canvas.setBitmap(bitmap)
+        drawable.setBounds(0,0,70,150)
+        drawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() { // CIERRA APLICACION O PASAMOS A OTRA ACTIVITY
+        super.onDestroy()
+        easyWayLocation?.endUpdates()
+    }*/
+
 }
